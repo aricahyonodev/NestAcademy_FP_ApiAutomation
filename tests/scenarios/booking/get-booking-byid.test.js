7bd64e124550f66d87485aea9b627e4d70b6a0bb
Ari Cahyono <cahyono.ari81@gmail.com>
2022-12-22 16:40:42 +0700

    Add: Negative case in get booking by id

--- tests/scenarios/booking/get-booking-byid.test.js
+++ tests/scenarios/booking/get-booking-byid.test.js
@@ -1,6 +1,7 @@
 import { assert } from "chai";
 import bookerApi from "$root/pages/booker.api";
 import * as data from "$root/data/booking.data";
+import * as res_code from "$root/data/res-status.data";
 
 describe("Get Booking By Id Positif Case", () => {
   let bookingid = null;
@@ -13,11 +14,26 @@ describe("Get Booking By Id Positif Case", () => {
 
     it("successful get booking by id registered", async () => {
       const res = await bookerApi.getBookingById(bookingid);
-      assert.equal(res.status, 200);
+      assert.equal(res.status, res_code.STATUS.OK);
       assert.hasAllDeepKeys(res.data, Object.keys(data.BOOKING));
       assert.hasAllDeepKeys(
         res.data.bookingdates,
         Object.keys(data.BOOKING.bookingdates)
       );
     });
+});
+describe("Get Booking By Id Negative Case", () => {
+ 
+    it("get booking by id not registered", async () => {
+      const res = await bookerApi.getBookingById(data.BOOKING_ID_NOT_REGISTERED);
+      assert.equal(res.status, 404);
+      assert.equal(res.data, "Not Found");
+    });
+
+    it("get booking by id string", async () => {
+      const res = await bookerApi.getBookingById(data.BOOKING_ID_STRING);
+      assert.equal(res.status, res_code.STATUS.NOT_FOUND);
+      assert.equal(res.data, "Not Found");
+    });
+
 });
